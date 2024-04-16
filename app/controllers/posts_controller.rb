183fc84449125ee86a27ee9257320f9cc764863f
benzy78 <westvillage815@gmail.com>
2024-04-16 23:45:28 +0900

    新規投稿機能の実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,8 +6,21 @@ class PostsController < ApplicationController
   end
 
   def new
+    @post = Post.new
+  end
+
+  def create
+    post = Post.new(post_params)
+    post.save!
+    redirect_to posts_url, notice: "投稿されました。"
   end
 
   def edit
+  end
+
+  # Strong Parameters : 不正なデータの登録・更新を防ぐ処理
+  private
+  def post_params
+    params.require(:post).permit(:name, :description)
   end
 end
