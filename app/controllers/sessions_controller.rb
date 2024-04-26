0ab637c1734a5f3d678ae7027dde749a77233f15
benzy78 <westvillage815@gmail.com>
2024-04-27 00:18:53 +0900

    ユーザーidとの関連を削除（暫定的な処理）

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -11,7 +11,7 @@ class SessionsController < ApplicationController
       session[:user_id] = user.id
       redirect_to root_url, notice:"ログインしました"
     else
-      render :new
+      render :new, status: :unprocessable_entity
     end
   end
 
